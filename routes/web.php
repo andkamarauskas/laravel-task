9b65c3fb932f7ed83bf59271704a36ccd6432da3
Andrius Kamarauskas <andkamarauskas@gmail.com>
2017-10-10 17:00:50 +0300

    fixed bug

--- routes/web.php
+++ routes/web.php
@@ -27,25 +27,3 @@ Route::group(['middleware' => ['auth']], function() {
 	Route::post('/device/store', 'DeviceController@store')->name('device.store');
 });
 
-Route::get('/map', function(){
-
-  $latitudeFrom = 54.8374570;
-  $longitudeFrom = 25.2619090;
-  $latitudeTo = 54.6883100;
-  $longitudeTo = 25.2612820;
-  $earthRadius = 6371000;
-
-  // convert from degrees to radians
-  $latFrom = deg2rad($latitudeFrom);
-  $lonFrom = deg2rad($longitudeFrom);
-  $latTo = deg2rad($latitudeTo);
-  $lonTo = deg2rad($longitudeTo);
-
-  $latDelta = $latTo - $latFrom;
-  $lonDelta = $lonTo - $lonFrom;
-
-  $angle = 2 * asin(sqrt(pow(sin($latDelta / 2), 2) + cos($latFrom) * cos($latTo) * pow(sin($lonDelta / 2), 2)));
-  $distance = $angle * $earthRadius;
-  echo $distance;
-
-});
